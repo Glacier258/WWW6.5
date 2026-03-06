6eb02e369170781148e092e261f9be006630d837
cca <c2063620237@gmail.com>
2026-03-06 23:31:41 +0800

    cca挑战尝试闯关到Day1

---
+++ day1-ClickCounter.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract ClickCounter{
+
+    uint256 public counter;
+
+    function Click() public{
+        counter ++;
+    }
+
+    function Reset() public{
+        counter = 0;
+    }
+}
