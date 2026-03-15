ec45852219240bc37992b101e4926e543b653790
cca <c2063620237@gmail.com>
2026-03-15 09:31:13 +0000

    day12_SimpleERC20.sol

--- cca/day12_SimpleERC20.sol
+++ cca/day12_SimpleERC20.sol
@@ -51,3 +51,16 @@ contract SimpleERC20 {
     }
 
 }
+/*
+//使用OpenZeppelin 轻松创建您自己的代币的方法
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+}
+
+*/
